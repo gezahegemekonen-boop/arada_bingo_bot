f69bdeb608c8f2050ab97a12bd9fc023afb0316c
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-25 01:02:51 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  const url = process.env.DB_URL;
+  if (!url) {
+    console.error("Missing DB_URL env var");
+    process.exit(1);
+  }
+  await mongoose.connect(url, { });
+  console.log("MongoDB connected");
+}
+
+module.exports = { connectDB };
 
