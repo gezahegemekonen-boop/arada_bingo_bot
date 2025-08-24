101e0b1abf31d656d6fd26f60ff0eff8bfc75d04
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-25 02:10:39 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
+
+const gameSchema = new mongoose.Schema({
+  chatId: String,
+  players: [String],
+  numbersCalled: [Number],
+  isActive: { type: Boolean, default: true },
+  jackpot: { type: Number, default: 0 },
+  bonus: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("Game", gameSchema);
 
