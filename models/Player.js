39b0ebf9784570eff7f396e62e3be1d7467532ac
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-25 02:13:20 +0300

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,19 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const playerSchema = new mongoose.Schema({
-  tgId: { type: String, unique: true, index: true },
-  name: String,
+  userId: String,
   username: String,
-  createdAt: { type: Date, default: Date.now },
-  wallet: { type: Number, default: 0 },
-  playWallet: { type: Number, default: 0 },
-  referredBy: { type: String, default: null },
-  hasClaimedReferral: { type: Boolean, default: false },
-  lastDailyBonusAt: { type: Date, default: null },
-  wins: { type: Number, default: 0 },
-  earnings: { type: Number, default: 0 },
-  stakeGroup: { type: Number, default: null }
+  score: { type: Number, default: 0 }
 });
 
-module.exports = mongoose.model('Player', playerSchema);
+module.exports = mongoose.model("Player", playerSchema);
+
 
