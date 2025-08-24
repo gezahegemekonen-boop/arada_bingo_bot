a73a465bc9807afaef3707976444686ed4122cee
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-25 02:22:41 +0300

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const playerSchema = new mongoose.Schema({
-  userId: String,
-  username: String,
-  score: { type: Number, default: 0 }
+  userId: { type: String, required: true },   // Telegram user ID
+  username: String,                           // Telegram username
+  balance: { type: Number, default: 0 },      // Player’s wallet balance
+  score: { type: Number, default: 0 },        // Points earned
+  gamesPlayed: { type: Number, default: 0 },  // Total games played
+  jackpotWins: { type: Number, default: 0 }   // Count of jackpot wins
 });
 
 module.exports = mongoose.model("Player", playerSchema);
-
-
