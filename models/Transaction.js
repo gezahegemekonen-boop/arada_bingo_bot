d2d905725fdd7588a34a5f62fd1d0660372b5d05
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-25 02:24:23 +0300

    Create Transaction.js

---
+++ models/Transaction.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  userId: { type: String, required: true },   // Player making tx
+  type: { type: String, enum: ["deposit", "withdraw"], required: true },
+  amount: { type: Number, required: true },
+  method: String,                             // CBE, Telebirr, etc.
+  status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
