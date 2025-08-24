1fb8981fe8fac7a39e281d6da69c3fdc9146752b
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-25 02:42:48 +0300

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  userId: { type: String, required: true },   // Player making tx
+  userId: { type: String, required: true },   // Player making transaction
   type: { type: String, enum: ["deposit", "withdraw"], required: true },
   amount: { type: Number, required: true },
   method: String,                             // CBE, Telebirr, etc.
