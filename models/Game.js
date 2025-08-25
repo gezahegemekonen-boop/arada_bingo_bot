c631f088d914eb5674c089c3388f275455c023a3
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-26 00:11:26 +0300

    Update Game.js

--- models/Game.js
+++ models/Game.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 const gameSchema = new mongoose.Schema({
-  chatId: String,                  // Telegram group chat ID
-  players: [String],                // Array of player IDs/usernames
-  numbersCalled: [Number],          // Numbers already called
-  isActive: { type: Boolean, default: true },
-  jackpot: { type: Number, default: 0 },
-  bonus: { type: Number, default: 0 }
+  startedAt: { type: Date, default: Date.now },
+  endedAt: { type: Date },
+  players: [{ type: mongoose.Schema.Types.ObjectId, ref: "Player" }],
+  winner: { type: mongoose.Schema.Types.ObjectId, ref: "Player" },
 });
 
 module.exports = mongoose.model("Game", gameSchema);
