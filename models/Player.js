7c1ca0b902ebd6496502686ec1919d4527cbc486
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 00:36:51 +0300

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
+
+const PlayerSchema = new mongoose.Schema({
+  telegramId: String,
+  balance: { type: Number, default: 0 },
+  cards: [Array],
+  wins: { type: Number, default: 0 }
+});
+
+export default mongoose.model("Player", PlayerSchema);
 
