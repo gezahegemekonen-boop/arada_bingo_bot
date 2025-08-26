becfcd19d9471973a75d8dff975e18c6b92f830e
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 00:29:44 +0300

    Update Game.js

--- models/Game.js
+++ models/Game.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const gameSchema = new mongoose.Schema({
-  startedAt: { type: Date, default: Date.now },
-  endedAt: { type: Date },
-  players: [{ type: mongoose.Schema.Types.ObjectId, ref: "Player" }],
-  winner: { type: mongoose.Schema.Types.ObjectId, ref: "Player" },
-});
-
-module.exports = mongoose.model("Game", gameSchema);
