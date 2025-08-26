9fe7d661f41d818cc3eccdda6720c43d397365bd
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 00:31:13 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,1 @@
-const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true, unique: true },
-  username: { type: String },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("User", userSchema);
