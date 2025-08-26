62d7c3169697bc4a5794f4db63aa218a3c0397ac
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 00:30:53 +0300

    Update Tx.js

--- models/Tx.js
+++ models/Tx.js
@@ -1,14 +1,1 @@
-const mongoose = require('mongoose');
-
-const txSchema = new mongoose.Schema({
-  tgId: String,
-  type: { type: String, enum: ['deposit','withdraw','payout','commission','jackpot','referral','daily'], required: true },
-  amount: Number,
-  method: { type: String, default: null },
-  status: { type: String, enum: ['pending','approved','rejected','done'], default: 'pending' },
-  note: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Tx', txSchema);
 
