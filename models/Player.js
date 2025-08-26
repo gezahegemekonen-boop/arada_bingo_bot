ba50de723b90641edcf700f5b127ad553055ec4c
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 00:30:05 +0300

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const playerSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true }, // Telegram user ID
-  username: { type: String },
-  balance: { type: Number, default: 0 },
-  joinedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Player", playerSchema);
