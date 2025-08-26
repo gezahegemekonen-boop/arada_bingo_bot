606e8332cafc7252bea308b738c8362d0535d38b
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 02:11:04 +0300

    Create admin.js

---
+++ src/handlers/admin.js
@@ -0,0 +1,17 @@
+const { createCanvas } = require('canvas');
+
+function renderCard(cardData) {
+  const canvas = createCanvas(300, 300);
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, 300, 300);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '20px Arial';
+  cardData.forEach((num, i) => {
+    ctx.fillText(num, 50 + (i % 5) * 40, 50 + Math.floor(i / 5) * 40);
+  });
+
+  return canvas.toBuffer('image/png');
+}
