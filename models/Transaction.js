d60bd543632901e5571355e88b10bc3f953cfad5
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 03:28:07 +0300

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,1 +1,13 @@
+// models/Transaction.js
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  type: { type: String, enum: ['deposit', 'withdraw'], required: true },
+  amount: Number,
+  playerId: String,
+  approved: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
 
