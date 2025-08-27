d335b6cc0913ab45fcca00454e0aa6e20955c880
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-28 00:47:51 +0300

    Create amharicAudioMap.js

---
+++ utils/amharicAudioMap.js
@@ -0,0 +1,13 @@
+module.exports = {
+  B: {
+    1: 'audio/amharic/B/B1.mp3',
+    2: 'audio/amharic/B/B2.mp3',
+    5: 'audio/amharic/B/B5.mp3',
+    // ... up to 15
+  },
+  I: {
+    16: 'audio/amharic/I/I16.mp3',
+    // ...
+  },
+  // Continue for N, G, O
+};
