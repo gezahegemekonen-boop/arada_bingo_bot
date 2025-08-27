762e09d9937a2f781923df40deee169df6568e1f
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-27 07:15:08 -0700

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -8,12 +8,32 @@ const playerSchema = new mongoose.Schema({
   },
   balance: {
     type: Number,
-    default: 0,
+    default: 10, // First-time bonus
   },
   isAdmin: {
     type: Boolean,
     default: false,
+  },
+  currentStake: {
+    type: Number,
+    default: null, // e.g. 10, 20, 50 ETB
+  },
+  lastJoinedRound: {
+    type: String,
+    default: null, // Used to prevent double-charging
+  },
+  card: {
+    type: mongoose.Schema.Types.Mixed,
+    default: null, // Stores assigned Bingo card
+  },
+  language: {
+    type: String,
+    default: 'en', // 'am' for Amharic
+  },
+  isApproved: {
+    type: Boolean,
+    default: true, // For deposit approval flow
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Player', playerSchema);
