f8d6eff632174c1bb616f7b0a500cf0df40d606c
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-30 07:50:00 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,17 @@ require('dotenv').config();
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 // 🎮 Player Commands
-require('./commands/player/help')(bot);
-require('./commands/player/transaction')(bot);
-require('./commands/player/balance')(bot);
-require('./commands/player/language')(bot);
+require('./commands/player/start')(bot);         // Auto-create player + welcome
+require('./commands/player/help')(bot);          // Show bilingual menu
+require('./commands/player/balance')(bot);       // Show wallet + coins
+require('./commands/player/language')(bot);      // Toggle Amharic/English
+require('./commands/player/transaction')(bot);   // Show deposit/withdraw history
 
 // 💵 Payment Commands
-require('./commands/deposit')(bot);
-require('./commands/confirm')(bot);
-require('./commands/withdraw')(bot);
-require('./commands/account')(bot);
+require('./commands/deposit')(bot);              // Handle deposits
+require('./commands/confirm')(bot);              // Admin confirms deposits
+require('./commands/withdraw')(bot);             // Handle withdrawals
+require('./commands/account')(bot);              // Show account info
 
 // Optional: log startup
 console.log('🤖 Bingo bot is running...');
