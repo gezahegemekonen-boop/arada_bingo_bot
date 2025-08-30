0d953ab969523e260c95a84988e74f5a93f97283
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-31 00:22:09 +0300

    Create BingoRound.js

---
+++ bingo-backend/ models/BingoRound.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const bingoRoundSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  roundId: { type: String, required: true },
+  card: { type: [[mongoose.Schema.Types.Mixed]], required: true }, // 5x5 grid
+  joinedAt: { type: Date, default: Date.now },
+  hasWon: { type: Boolean, default: false },
+  winType: { type: String, enum: ["row", "column", "diagonal", "corners", null], default: null },
+  stake: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('BingoRound', bingoRoundSchema);
