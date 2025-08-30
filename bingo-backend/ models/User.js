871df705904d9ce9b2615ff7ecee95e97fae85d3
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-30 05:59:14 -0700

    Update User.js

--- bingo-backend/ models/User.js
+++ bingo-backend/ models/User.js
@@ -1,1 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  gamesPlayed: { type: Number, default: 0 },
+  wins: { type: Number, default: 0 },
+  lastPlayedAt: { type: Date, default: null }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
 
