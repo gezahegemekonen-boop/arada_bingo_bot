0222ba633c2be4d56e588a4b84c72dfd9885bd32
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-08-31 02:41:50 -0700

    Update App.js

--- bingo-frontend/src/App.js
+++ bingo-frontend/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import LiveGameView from './views/LiveGameView/LiveGameView';
+
+function App() {
+  return (
+    <div className="App">
+      <LiveGameView />
+    </div>
+  );
+}
+
+export default App;
 
