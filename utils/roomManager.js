dd9834f3ed3e9be1cd4f5d94203a171e1a9002ac
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-03 00:12:51 +0300

    Update roomManager.js

--- utils/roomManager.js
+++ utils/roomManager.js
@@ -2,21 +2,36 @@
 
 const rooms = {}; // key: roomId, value: room object
 
-function getRoomByPlayerId(playerId) {
-  return Object.values(rooms).find(room => room.players[playerId]);
-}
-
 function getRoom(roomId) {
-  return rooms[roomId];
+  return rooms[roomId] || null;
 }
 
-function addRoom(roomId, roomData) {
-  rooms[roomId] = roomData;
+function getRoomByPlayerId(playerId) {
+  return Object.values(rooms).find(room => room.players && room.players[playerId]);
+}
+
+function addRoom(roomId, roomData = {}) {
+  if (!roomId) throw new Error('Room ID is required');
+  rooms[roomId] = {
+    ...roomData,
+    players: roomData.players || {},
+    createdAt: new Date()
+  };
+}
+
+function removeRoom(roomId) {
+  delete rooms[roomId];
+}
+
+function listRooms() {
+  return Object.keys(rooms);
 }
 
 module.exports = {
-  getRoomByPlayerId,
   getRoom,
+  getRoomByPlayerId,
   addRoom,
+  removeRoom,
+  listRooms,
   rooms
 };
