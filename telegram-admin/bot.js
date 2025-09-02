153d1d296dc8534d3467230c6f01f68c853730a1
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-02 05:19:17 -0700

    Update bot.js

--- telegram-admin/bot.js
+++ telegram-admin/bot.js
@@ -1,1 +1,21 @@
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+import handlePendingDeposits from './commands/pendingDeposits.js';
+import handlePendingPayouts from './commands/pendingPayouts.js';
+import handleCallback from './commands/callbackHandler.js';
+
+dotenv.config();
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.onText(/\/pending_deposits/, (msg) => {
+  handlePendingDeposits(bot, msg.chat.id);
+});
+
+bot.onText(/\/pending_payouts/, (msg) => {
+  handlePendingPayouts(bot, msg.chat.id);
+});
+
+bot.on('callback_query', (query) => {
+  handleCallback(bot, query);
+});
 
