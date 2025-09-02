aac1eced6f4b866fb2b9a9b1e0fa2cb526076fd6
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-02 05:17:30 -0700

    Update apiClient.js

--- telegram-admin/services/apiClient.js
+++ telegram-admin/services/apiClient.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const api = axios.create({
+  baseURL: process.env.BACKEND_URL,
+  headers: {
+    'x-admin-token': process.env.ADMIN_SECRET
+  }
+});
+
+export default api;
 
