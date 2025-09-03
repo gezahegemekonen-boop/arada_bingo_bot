e4c683bc5e91cd54bce698a3836829ba6b7f44a2
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-04 02:16:08 +0300

    Update Player.js

--- bingo-backend/ models/Player.js
+++ bingo-backend/ models/Player.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const playerSchema = new mongoose.Schema({
   telegramId: { type: String, required: true, unique: true },
   balance: { type: Number, default: 0 },
   coins: { type: Number, default: 0 },
-  language: { type: String, enum: ['am', 'en'], default: 'am' },
+  language: { type: String, enum: ['am','en'], default: 'am' },
   lastPlayed: { type: Date },
   isAdmin: { type: Boolean, default: false }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Player', playerSchema);
+export default mongoose.model('Player', playerSchema);
