fef8eaea6abc5b5d84bef0c2e78867ee66d5c63f
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-04 01:29:03 +0300

    Update generateCard.js

--- bingo-backend/routes/helpers/generateCard.js
+++ bingo-backend/routes/helpers/generateCard.js
@@ -1,1 +1,14 @@
+module.exports = function generateCard() {
+  const card = [];
+  for (let i = 0; i < 5; i++) {
+    const row = [];
+    for (let j = 0; j < 5; j++) {
+      row.push(Math.floor(Math.random() * 100) + 1);
+    }
+    card.push(row);
+  }
+  // Optional: free center
+  card[2][2] = 'FREE';
+  return card;
+};
 
