335b55f38321f82c88de41b74a3b764a0e065224
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-04 01:36:48 +0300

    Update generateCard.js

--- bingo-backend/helpers/generateCard.js
+++ bingo-backend/helpers/generateCard.js
@@ -1,32 +1,14 @@
-// helpers/generateCard.js
-
-function generateCard() {
+module.exports = function generateCard() {
   const card = [];
-  const columns = [
-    { start: 1, end: 15 },
-    { start: 16, end: 30 },
-    { start: 31, end: 45 },
-    { start: 46, end: 60 },
-    { start: 61, end: 75 }
-  ];
-
-  for (let col = 0; col < columns.length; col++) {
-    const nums = [];
-    while (nums.length < 5) {
-      const num = Math.floor(Math.random() * (columns[col].end - columns[col].start + 1)) + columns[col].start;
-      if (!nums.includes(num)) nums.push(num);
+  for (let i = 0; i < 5; i++) {
+    const row = [];
+    for (let j = 0; j < 5; j++) {
+      row.push(Math.floor(Math.random() * 100) + 1);
     }
-    card.push(nums);
+    card.push(row);
   }
-
-  const transposed = [];
-  for (let row = 0; row < 5; row++) {
-    transposed[row] = card.map(col => col[row]);
-  }
-
-  transposed[2][2] = 'FREE';
-  return transposed;
-}
-
-module.exports = generateCard;
+  // Optional: free center
+  card[2][2] = 'FREE';
+  return card;
+};
 
