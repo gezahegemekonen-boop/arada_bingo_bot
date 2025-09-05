958c2738b5d6731a1583a192370d024d358daca6
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-06 01:12:00 +0300

    Update Player.js

--- src/bingo-backend/models/Player.js
+++ src/bingo-backend/models/Player.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoosefrom 'mongoose';
 
 const PlayerSchema = new mongoose.Schema({
   telegramId: { type: String, required: true, unique: true, index: true },
