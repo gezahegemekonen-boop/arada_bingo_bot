d7c784a8e37e9f385068636e8026127634d1cfeb
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-05 04:47:56 -0700

    Update playController.js

--- bingo-backend/controllers/playController.js
+++ bingo-backend/controllers/playController.js
@@ -1,1 +1,12 @@
 
+export const getAllPlayers = (req, res) => {
+  res.send('All players');
+};
+
+export const getPlayer = (req, res) => {
+  res.send('Single player');
+};
+
+export const updatePlayer = (req, res) => {
+  res.send('Player updated');
+};
