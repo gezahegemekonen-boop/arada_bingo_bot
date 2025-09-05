e63b5728be06638c41d5ba222648e0d569f05e72
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-06 00:42:18 +0300

    Update Player.js

--- src/models/Player.js
+++ src/models/Player.js
@@ -1,18 +1,1 @@
-import mongoose from 'mongoose';
 
-const PlayerSchema = new mongoose.Schema({
-  telegramId: { type: String, index: true, required: true, unique: true },
-  username: String,
-  balance: { type: Number, default: 0 },
-  language: { type: String, enum: ['en', 'am'], default: 'en' },
-  banned: { type: Boolean, default: false },
-  wins: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-
-  // ✅ Referral system fields
-  referralCode: { type: String },
-  referrals: [{ type: String }],
-  referralCoins: { type: Number, default: 0 }
-});
-
-export default mongoose.model('Player', PlayerSchema);
