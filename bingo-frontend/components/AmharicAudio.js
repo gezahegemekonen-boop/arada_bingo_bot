f3c7450da2677518e58968d77931726f14949a83
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-05 07:17:10 -0700

    Update AmharicAudio.js

--- bingo-frontend/components/AmharicAudio.js
+++ bingo-frontend/components/AmharicAudio.js
@@ -1,1 +1,11 @@
+// components/AmharicAudio.js
+export function playAmharicAudio(type) {
+  const audio = new Audio();
+  if (type === 'win') {
+    audio.src = 'assets/sounds/win.mp3';
+  } else if (type === 'lose') {
+    audio.src = 'assets/sounds/lose.mp3';
+  }
+  audio.play();
+}
 
