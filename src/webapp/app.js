bc184776dbbfeea1046801e9ffc235641bd0867a
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-06 07:22:13 -0700

    Update app.js

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -1,1 +1,23 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+const userId = tg.initDataUnsafe?.user?.id;
+const username = tg.initDataUnsafe?.user?.username;
+
+document.getElementById('welcome').innerText = `👋 Welcome, ${username || 'Player'}!`;
+
+fetch(`https://bingo-backend-vdeo.onrender.com/referral/${userId}`)
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      document.getElementById('referral').innerText =
+        `Your referral code: ${data.referralCode}\nReferrals: ${data.referrals}\nCoins: ${data.coinsEarned}`;
+    } else {
+      document.getElementById('referral').innerText = 'Referral data not found.';
+    }
+  });
+
+document.getElementById('inviteBtn').onclick = () => {
+  tg.openTelegramLink(`https://t.me/your_bot_username?start=${userId}`);
+};
 
